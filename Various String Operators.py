d43d636539b59ca4ec1ddeeb4d6935387adccb3f
Mohammad Ali <smdali88@gmail.com>
2019-06-01 17:53:26 +0530

    operators and all string files

---
+++ Various String Operators.py
@@ -0,0 +1,152 @@
+"""
+a = "ali"
+b = 2
+print(a+str(b))
+"""
+
+#suare brackets[]
+"""
+x="Guru"
+print(x[1])
+"""
+
+#[ : ]
+"""
+x="Guru" 
+print(x[1:3])
+"""
+
+#in
+"""
+x="Guru" 
+print("u" in x)
+"""
+
+#not in
+"""
+x="Guru" 
+print("a" in x)
+"""
+
+#r/R
+"""
+Print(r'\n')
+prints(\n):
+print(R'/n')
+prints(\n)
+"""
+
+#% - Used for string format
+"""
+name = "guru"
+number = 99
+print('%s %d' % (name,number))
+"""
+
+#+
+"""
+x="Guru" 
+y="99" 
+print(x+y)
+"""
+
+#*
+"""
+x="Guru" 
+y="99" 
+print(x*2)
+"""
+
+"""
+x="ali"
+y="357"
+print(y*2)
+"""
+
+"""
+x = "Hello World!"
+print(x[:6]) 
+print(x[0:6] + "Guru99")
+"""
+
+#Python String replace() Method
+"""
+oldstring = 'I like Guru99' 
+newstring = oldstring.replace('like', 'love')
+print(newstring)
+"""
+
+#Changing upper and lower case strings
+"""
+string="python at guru99"
+print(string.upper())
+"""
+
+"""
+string="python at guru99"		
+print(string.capitalize())
+"""
+
+"""
+string="PYTHON AT GURU99"
+print(string.lower())
+"""
+
+#Using "join" function for the string
+
+"""
+print(":".join("Python"))
+"""
+
+#Reversing String
+"""
+string="12345"		
+print(''.join(reversed(string)))
+"""
+
+#Split Strings
+"""
+word="guru99 career guru99"		
+print(word.split(' '))
+"""
+
+"""
+word="guru99 career guru99"		
+print(word.split('r'))
+"""
+
+"""
+Important Note:
+
+In Python, Strings are immutable.
+
+"""
+
+"""
+x = "Guru99"
+x.replace("Guru99","Python")
+print(x)
+
+x = "Guru99"
+x = x.replace("Guru99","Python")
+print(x)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
